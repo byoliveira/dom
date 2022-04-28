96204fb33cf922f83c72a0f5af5304a5844fdbd4
oliveira.bruno <oliveira.bruno@greenbeltti.onmicrosoft.com>
2022-04-28 10:44:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//getElementById()
+
+/* const element = document.getElementById('elemento')
+
+console.log(element); */
+
+/* const element = document.getElementsByClassName('elemento')
+
+console.log(element); */
+
+/* const element = document.getElementsByTagName('meta')
+
+console.log(element); */
+
+const element = document.querySelector('h1')
+
+element.innerText = 'Mudando texto'
+
+console.log(element.textContent);
