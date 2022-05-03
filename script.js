73185d1e443ca36c652d8a4f22b0dfc1e2c6e357
oliveira.bruno <oliveira.bruno@greenbeltti.onmicrosoft.com>
2022-05-03 10:48:49 -0300

    feat: Eventos

--- script.js
+++ script.js
@@ -16,9 +16,33 @@ console.log(element); */
 
 element.innerHTML = 'Mudando texto <small>adicionando small</small>' */
 
-const element = document.querySelector('input')
+/* const element = document.querySelector('input')
 
 
 element.value = "Texto base";
 
-console.log(element);
+console.log(element); */
+
+/* function print() {
+  console.log('print');
+} */
+
+/* const input = document.querySelector('input')
+
+input.onkeydown = function() {
+  console.log('Estou dentro do evento');
+} */
+
+/* const h1 = document.querySelector('h1')
+
+h1.addEventListener('mouseover', print)
+
+function print() {
+  console.log('print');
+} */
+
+const input = document.querySelector('input')
+
+input.onkeydown = function(event) {
+  console.log(event);
+}
